055b890e094ad17cf344f9ec68c212c71887d330
Sagar Garg <sagargupta1417@gmail.com>
2020-04-30 15:06:46 +0530

    additional changes

---
+++ script.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("popup");
+var btn = document.getElementById("btn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
