49aed3870a7cce8d1bcfbb4e0ce5675689ab2a83
Sagar Garg <sagargupta1417@gmail.com>
2020-05-08 16:34:44 +0530

    update experience and education section

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ window.onclick = function(event) {
   }
 }
 
+// // smooth scroll section
+//  var navMenuAnchorTags = document.querySelectorAll('nav a')
+
+// for(let i =0;i<navMenuAnchorTags.length;i++){
+//   navMenuAnchorTags[i].addEventListener('click', function(e){
+//     e.preventDefault();
+//     var targetSectionID = this.textContent.trim().toLowerCase();
+//     var targetSection = document.getElementById(targetSectionID);
+
+//     var interval = setInterval(function(){
+      
+//       window.scrollBy(0,50);
+//     }, 50);
+//   })
+// }
