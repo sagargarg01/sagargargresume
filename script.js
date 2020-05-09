f946fb6138af260e7f69cf24e3e9244cb9ae09da
Sagar Garg <sagargupta1417@gmail.com>
2020-05-09 10:38:42 +0530

    testing

--- script.js
+++ script.js
@@ -19,19 +19,3 @@ window.onclick = function(event) {
     document.body.style.overflow = "scroll";
   }
 }
-
-// // smooth scroll section
-//  var navMenuAnchorTags = document.querySelectorAll('nav a')
-
-// for(let i =0;i<navMenuAnchorTags.length;i++){
-//   navMenuAnchorTags[i].addEventListener('click', function(e){
-//     e.preventDefault();
-//     var targetSectionID = this.textContent.trim().toLowerCase();
-//     var targetSection = document.getElementById(targetSectionID);
-
-//     var interval = setInterval(function(){
-      
-//       window.scrollBy(0,50);
-//     }, 50);
-//   })
-// }
