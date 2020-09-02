373bfb7333d596e96d1062feeba8065d22757677
Sagar Garg <sagargupta1417@gmail.com>
2020-09-02 08:43:26 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Loader 
 minimumLoading = () =>{
   setTimeout(() => {
     console.log('page is fully loaded');
@@ -5,7 +6,7 @@ minimumLoading = () =>{
   }, 4000);
 }
 
-remoevLoader = () =>{
+removeLoader = () =>{
   setTimeout(() => {
     document.getElementById('loading').style.display = 'none';
   },5000 );
@@ -14,11 +15,12 @@ remoevLoader = () =>{
 window.onload = (event) => {
 
   minimumLoading();
-  remoevLoader();
+  removeLoader();
   
 };
+// --------------------------------------------------------------------------------
 
-
+// Handling Pop-Up On Mobile View
 var modal = document.getElementById("popup");
 var btn = document.getElementById("btn");
 var span = document.getElementsByClassName("close")[0];
@@ -40,11 +42,48 @@ window.onclick = function(event) {
     document.body.style.overflow = "scroll";
   }
 }
+//---------------------------------------------------------------------------------
 
-
+// increase the height of message box automatically
 $('textarea').each(function () {
   this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
 }).on('input', function () {
   this.style.height = 'auto';
   this.style.height = (this.scrollHeight) + 'px';
 });
+// --------------------------------------------------------------------------------
+
+// hide the url from showing
+$(function(){
+  
+  $("a").each(function (index, element){
+      var href = $(this).attr("href");
+      $(this).attr("hiddenhref", href);
+      $(this).removeAttr("href");
+  });
+  $(".social-icons a").click(function(){
+      url = $(this).attr("hiddenhref");
+      window.open(url, '_blank');
+  })
+  $(".modal-container a, .nav-menu a , .otherLinks").click(function(){
+    url = $(this).attr("hiddenhref");
+    window.location.href = url;
+  })
+});
+// --------------------------------------------------------------------------------
+
+//submit form
+$('form').submit(function(e){
+  e.preventDefault();
+  let name = $('#SenderName').val();
+  let subject = $('#SenderSubject').val();
+  let message = $('#SenderMessage').val().replace(/(\r\n|\n|\r)/gm, '%0D%0A');
+ 
+  location.href = "mailto:sagargupta1417@gmail.com" +
+    ("?subject=" + subject) + 
+    ("&body=" + message);
+
+    $('#SenderName').val('');
+    $('#SenderSubject').val('');
+    $('#SenderMessage').val('');
+})
